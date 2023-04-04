28a1618c3eb406f317441ee1696fdba0b141e880
Daniel <daniel.inama@mailbox.org>
2023-04-04 19:02:53 +0200

    added cool animation

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ particlesJS.load("particles-js", "assets/particles.json", function () {
 
 
 window.onload = function () {
+
+
+	// Cursor
+
 	const cursor = document.querySelector(".cursor");
 
 	document.body.onpointermove = event => {
@@ -23,4 +27,39 @@ window.onload = function () {
 			easing: "ease-out"
 		});
 	}
+
+
+	// Title animation
+
+	const traveler = document.querySelector("#title_traveler");
+	const words = ["Explorer", "Voyager", "Adventurer", "Traveler", "Pioneer", "Discoverer", "Navigator", "Pathfinder", "Wayfarer", "Pilgrim", "Journeyer", "Wanderer", "Nomad", "Trekker", "Visitor"];
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
+
+	let iterations = 0;
+	traveler.innerHTML = words[Math.floor(Math.random() * words.length)];
+	setInterval(() => {
+
+		let newWord = words[Math.floor(Math.random() * words.length)];
+
+		if (newWord === traveler.innerHTML) {
+			newWord = words[Math.floor(Math.random() * words.length)];	
+		}
+
+		const interval = setInterval(() => {
+			traveler.innerHTML = newWord.split("").map((letter, index) => {
+				if(index < iterations){
+					return newWord[index];
+				}
+				return letters[Math.floor(Math.random() * letters.length)
+			]}).join("");
+
+			if (iterations > newWord.length) {
+				clearInterval(interval);
+				traveler.innerHTML = newWord
+			}
+			iterations++;
+		}, 50);
+		iterations = 0;
+	}, 5000);
+	
 }
